c29419e1493caf461b354dc31124c624633af424
Rayn.Gosling <wenleigang0926@163.com>
2018-04-17 11:19:14 +0800

    add

--- soccer/soccer_backend/src/main/java/com/chaosquark/backend/common/constant/Constants.java
+++ soccer/soccer_backend/src/main/java/com/chaosquark/backend/common/constant/Constants.java
@@ -1,4 +1,30 @@
 package com.chaosquark.backend.common.constant;
 
-public class Constants {
+public interface Constants {
+
+    String CONTEXT_PATH = "ctx";
+
+    /*------------------------------ 上一次请求地址 -------------------------*/
+    String BACK_URL = "backURL";
+
+    /*------------------------------ 当前登录用户 ---------------------------*/
+    String CURRENT_USER = "user";
+
+    /*------------------------------ 菜单 ----------------------------------*/
+    String MENU = "menu";
+
+    /*------------------------------ 验证码 --------------------------------*/
+    String VOLITION_CODE = "volitionCode";
+
+    /*------------------------------ 初始用户密码 ---------------------------*/
+    String DEFAULT_USER_PASSWORD = "111111";
+
+    /*------------------------------ 日期 ----------------------------------*/
+    String YMD_HMS = "yyyy-MM-dd HH:mm:ss";
+
+    String YMD_HM = "yyyy-MM-dd HH:mm";
+
+    String YMD = "yyyy-MM-dd";
+
+    String YM = "yyyy年MM月";
 }
